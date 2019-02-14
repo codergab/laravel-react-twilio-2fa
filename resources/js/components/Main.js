eb040930690b3100da736d71b86b90c13b31c412
Gabriel Adewumi <codergab@gmail.com>
2019-02-14 13:09:50 +0100

    to linux

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import ReactDOM from 'react-dom';
-import { Redirect } from 'react-router-dom';
+import {
+    Redirect
+} from 'react-router-dom';
 import AppRouter from './Router';
 import checkAuth from './Auth';
+
 export default class Main extends Component {
     componentWillMount() {
-        if(!checkAuth()) {
-            <Redirect to="/" />
+        if (!checkAuth()) {
+            <Redirect to = "/" / >
         }
     }
     render() {
-        
-        return (
+        return ( 
             <React.Fragment>
             <AppRouter />
-            
             </React.Fragment>
         );
     }
 }
 
 if (document.getElementById('app-root')) {
-    ReactDOM.render(<Main />, document.getElementById('app-root'));
+    ReactDOM.render( < Main / > , document.getElementById('app-root'));
 }
